131f04ba7f82f7a915dcb7a8a8b30bef9ec97cf9
Machbell91 <80841661+Machbell91@users.noreply.github.com>
2024-03-22 19:56:56 +0100

    Delete Documents directory

--- Documents/DBOP/dbop/android/app/src/main/kotlin/com/example/dbop/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dbop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Documents/newapp/finalapp/android/app/src/main/kotlin/com/example/finalapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.finalapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
